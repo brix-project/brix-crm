dfe59c8ddc106871153633d6670aa9f25b3bc2eb
Matthias Leuffen <matthes@leuffen.de>
2024-07-11 11:40:28 +0200

    sync

--- src/Actions/CustomerCreateAction.php
+++ src/Actions/CustomerCreateAction.php
@@ -3,6 +3,7 @@ namespace Brix\CRM\Actions;
 use Brix\Core\Broker\Broker;
 use Brix\Core\Broker\BrokerActionInterface;
 use Brix\Core\Broker\BrokerActionResponse;
+use Brix\Core\Broker\Message\ContextMsg;
 use Brix\CRM\Actions\Types\CustomerCreateActionRequestType;
 use Brix\CRM\Business\CustomerManager;
 use Brix\CRM\Type\Customer\T_CRM_Customer;
@@ -57,9 +58,14 @@ class CustomerCreateAction implements BrokerActionInterface
         $input = phore_cast_object($input, T_CRM_Customer::class);
 
         $customerManager->createCustomer($input);
-        $newContextId = $input->customerId . "-" . $input->customerSlug;
+        $newContextId = $input->customerId . "-" . strtolower($input->customerSlug);
+        $newSubscriptionId = strtolower($input->customerSlug . "-" . $input->customerId);
 
         $broker->contextStorageDriver->createContext($newContextId, "$input->address");
+
+        $broker->contextStorageDriver->withContext($newContextId)->processContextMsg(new ContextMsg("crm.customer_data", "The billing address of the customer. Use as main address if nothing other is specified", $input));
+        $broker->contextStorageDriver->withContext($newContextId)->processContextMsg(new ContextMsg("subscription_id", "The subscription_id for this customer.", $newSubscriptionId));
+        $broker->contextStorageDriver->withContext($newContextId)->processContextMsg(new ContextMsg("domain: abcde.de", "The subscription_id for this customer.", $newSubscriptionId));
         $ret = new BrokerActionResponse();
         $ret->status = "ok";
         $ret->message = "Customer created (ID: $input->customerId, Slug: $input->customerSlug, Context: $newContextId)";
